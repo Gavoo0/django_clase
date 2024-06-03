dc7e7c3b029ac89f7fd6f57d9177ff1803e3603f
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-06-03 10:18:56 -0400

    a

--- alumnos/alumnos/alumnos/urls.py
+++ alumnos/alumnos/alumnos/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('index',views.index,name='index'),
+    path('listadoSQL',views.listadoSQL,name='listadoSQL'),
+    path('crud', views.crud, name='crud'),
+    path('alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
+
 ]
--- alumnos/alumnos/alumnos/views.py
+++ alumnos/alumnos/alumnos/views.py
@@ -5,3 +5,19 @@ def index(request):
     alumnos=Alumno.objects.all()
     context={"alumnos":alumnos}
     return render(request,'alumnos/index.html',context)
+
+def listadoSQL(request):
+    alumnos=Alumno.objects.raw('SELECT * FROM alumnos_alumno')
+    context={"alumnos":alumnos}
+    return render(request,'alumnos/index.html',context)
+
+def crud(request):
+    alumnos=Alumno.objects.all()
+    context={'alumnos':alumnos}
+    return render(request,'alumnos/alumnos_list.html',context)
+
+def alumnosAdd(request):
+    if request.method is not "POST":
+        generos=Genero.objects.all()
+        context={'generos':generos}
+    return render(request,'alumnos/alumnos_add.html',context)
