9b5786b8bd07b239e6e94f17fb3c69aed8cb0ac5
Gavoo0 <gabrieli.godoyl@gmail.com>
2024-06-07 11:57:49 -0400

    cambio

--- alumnos/alumnos/alumnos/urls.py
+++ alumnos/alumnos/alumnos/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('listadoSQL',views.listadoSQL,name='listadoSQL'),
     path('crud', views.crud, name='crud'),
     path('alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
-
+    path('alumnos_del/<str:pk>',views.alumnos_del,name="alumnos_del"),
+    path('alumnos_findEdit/<str:pk>',views.alumnos_findEdit,name="alumnos_findEdit"),
+    path('alumnosUpdate', views.alumnosUpdate, name='alumnosUpdate'),
 ]
